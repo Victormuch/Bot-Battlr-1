a76025542fbf93b0b513ab1f88f21751652761d0
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-05-06 13:35:14 +0300

    sorted

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
+import SortBar from "./SortBar";
+
 function BotCollection() {
   const [bots, setBots] = useState([]);
   const [enlistedBots, setEnlistedBots] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState([]);
 
   useEffect(() => {
     fetch("https://my-json-server.typicode.com/Victormuch/Bot-Battlr-1/bots")
@@ -25,46 +28,61 @@ function BotCollection() {
 
   function handleDischarge(bot) {
     fetch(
-      `https://my-json-server.typicode.com/Victormuch/Bot-Battlr-1/bots${bot.id}`,
+      `https://my-json-server.typicode.com/Victormuch/Bot-Battlr-1/bots/${bot.id}`,
       {
         method: "DELETE",
       }
     );
   }
 
+  function handleSort1(e) {
+    const selectedSort = e.target.value;
+    let sortedBots = [...bots];
+
+    if (selectedSort === "health") {
+      sortedBots.sort((a, b) => b.health - a.health);
+    } else if (selectedSort === "damage") {
+      sortedBots.sort((a, b) => b.damage - a.damage);
+    } else if (selectedSort === "armor") {
+      sortedBots.sort((a, b) => b.armor - a.armor);
+    }
+
+    setBots(sortedBots);
+  }
+
+  const handleFilterChange = (e) => {
+    const selectedOptions = Array.from(e.target.selectedOptions).map(
+      (option) => option.value
+    );
+    setSelectedFilters(selectedOptions);
+
+    let filteredBots = [...bots];
+    filteredBots = filteredBots.filter((bot) =>
+      selectedOptions.includes(bot.bot_class)
+    );
+    setBots(filteredBots);
+  };
+
   return (
- <div className="bot-container">
-    <YourBotArmy
-      enlistedBots={enlistedBots}
-      onRelease={handleRelease}
-      onDischarge={handleDischarge}
-    />
-    <div className="card-container">
-      {bots.map((bot) => (
-        <div key={bot.id} className="card" onClick={() => enlistBot(bot)}>
-          <img src={bot.avatar_url} alt={bot.name} />
-          <h3>{bot.name}</h3>
-          <p className="catchphrase">{bot.catchphrase}</p>
-          
-          <span>
-            <i className="icon heartbeat" />
-            {bot.health}
-          </span>
-          <span>
-            <i className="icon lightning" />
-            {bot.damage}
-          </span>
-          <span>
-            <i className="icon shield" />
-            {bot.armor}
-          </span>
-          
-          <button>Enlist</button>
-        </div>
-      ))}
+    <div className="bot-container">
+      <SortBar onSort1={handleSort1} onFilterData={handleFilterChange} />
+      <YourBotArmy
+        enlistedBots={enlistedBots}
+        onRelease={handleRelease}
+        onDischarge={handleDischarge}
+      />
+      <div className="card-container">
+        {bots.map((bot) => (
+          <div key={bot.id} className="card" onClick={() => enlistBot(bot)}>
+            <img src={bot.avatar_url} alt={bot.name} />
+            <h3>{bot.name}</h3>
+            <p className="catchphrase">{bot.catchphrase}</p>
+            <button className="button">Enlist</button>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default BotCollection;
--- src/components/SortBar.jsx
+++ src/components/SortBar.jsx
@@ -1,5 +1,14 @@
-function SortBar() {
-  return <div>SortBar</div>;
-}
+const SortBar = ({ onSort1, onFilterData }) => {
+  return (
+    <div className="sort-bar">
+      <select onChange={onSort1}>
+        <option value="health">Sort by Health</option>
+        <option value="damage">Sort by Damage</option>
+        <option value="armor">Sort by Armor</option>
+      </select>
+     
+    </div>
+  );
+};
 
 export default SortBar;
